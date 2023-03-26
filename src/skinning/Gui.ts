0cb61fc62f83d9b9445ca25ce3325aa436171b85
jlq279 <joyce.l.qin@utexas.edu>
2023-03-26 17:04:59 -0500

    ye

---
+++ src/skinning/Gui.ts
@@ -0,0 +1,448 @@
+import { Camera } from "../lib/webglutils/Camera.js";
+import { CanvasAnimation } from "../lib/webglutils/CanvasAnimation.js";
+import { SkinningAnimation } from "./App.js";
+import { Mat4, Vec3, Vec4, Vec2, Mat2, Quat } from "../lib/TSM.js";
+import { Bone, Mesh } from "./Scene.js";
+import { RenderPass } from "../lib/webglutils/RenderPass.js";
+
+/**
+ * Might be useful for designing any animation GUI
+ */
+interface IGUI {
+  viewMatrix(): Mat4;
+  projMatrix(): Mat4;
+  dragStart(me: MouseEvent): void;
+  drag(me: MouseEvent): void;
+  dragEnd(me: MouseEvent): void;
+  onKeydown(ke: KeyboardEvent): void;
+}
+
+export enum Mode {
+  playback,  
+  edit  
+}
+
+/**
+ * Handles Mouse and Button events along with
+ * the the camera.
+ */
+
+export class GUI implements IGUI {
+  private static readonly rotationSpeed: number = 0.05;
+  private static readonly zoomSpeed: number = 0.1;
+  private static readonly rollSpeed: number = 0.1;
+  private static readonly panSpeed: number = 0.1;
+
+  private camera: Camera;
+  private dragging: boolean;
+  private fps: boolean;
+  private prevX: number;
+  private prevY: number;
+
+  private height: number;
+  private viewPortHeight: number;
+  private width: number;
+
+  private animation: SkinningAnimation;
+
+  public time: number;
+  
+  public mode: Mode;
+  
+
+  public hoverX: number = 0;
+  public hoverY: number = 0;
+
+  public highlightedBoneIndex: number = -1;
+
+
+  /**
+   *
+   * @param canvas required to get the width and height of the canvas
+   * @param animation required as a back pointer for some of the controls
+   * @param sponge required for some of the controls
+   */
+  constructor(canvas: HTMLCanvasElement, animation: SkinningAnimation) {
+    this.height = canvas.height;
+    this.viewPortHeight = this.height - 200;
+    this.width = canvas.width;
+    this.prevX = 0;
+    this.prevY = 0;
+    
+    this.animation = animation;
+    
+    this.reset();
+    
+    this.registerEventListeners(canvas);
+  }
+
+  public getNumKeyFrames(): number {
+    // TODO
+    // Used in the status bar in the GUI
+    return 0;
+  }
+  public getTime(): number { return this.time; }
+  
+  public getMaxTime(): number { 
+    // TODO
+    // The animation should stop after the last keyframe
+    return 0;
+  }
+
+  /**
+   * Resets the state of the GUI
+   */
+  public reset(): void {
+    this.fps = false;
+    this.dragging = false;
+    this.time = 0;
+    this.mode = Mode.edit;
+    this.camera = new Camera(
+      new Vec3([0, 0, -6]),
+      new Vec3([0, 0, 0]),
+      new Vec3([0, 1, 0]),
+      45,
+      this.width / this.viewPortHeight,
+      0.1,
+      1000.0
+    );
+  }
+
+  /**
+   * Sets the GUI's camera to the given camera
+   * @param cam a new camera
+   */
+  public setCamera(
+    pos: Vec3,
+    target: Vec3,
+    upDir: Vec3,
+    fov: number,
+    aspect: number,
+    zNear: number,
+    zFar: number
+  ) {
+    this.camera = new Camera(pos, target, upDir, fov, aspect, zNear, zFar);
+  }
+
+  /**
+   * Returns the view matrix of the camera
+   */
+  public viewMatrix(): Mat4 {
+    return this.camera.viewMatrix();
+  }
+
+  /**
+   * Returns the projection matrix of the camera
+   */
+  public projMatrix(): Mat4 {
+    return this.camera.projMatrix();
+  }
+
+  /**
+   * Callback function for the start of a drag event.
+   * @param mouse
+   */
+  public dragStart(mouse: MouseEvent): void {
+    if (mouse.offsetY > 600) {
+      // outside the main panel
+      return;
+    }
+    
+    // TODO
+    // Some logic to rotate the bones, instead of moving the camera, if there is a currently highlighted bone
+    
+    this.dragging = true;
+    this.prevX = mouse.screenX;
+    this.prevY = mouse.screenY;
+    
+  }
+
+  public incrementTime(dT: number): void {
+    if (this.mode === Mode.playback) {
+      this.time += dT;
+      if (this.time >= this.getMaxTime()) {
+        this.time = 0;
+        this.mode = Mode.edit;
+      }
+    }
+  }
+
+  /**
+   * The callback function for a drag event.
+   * This event happens after dragStart and
+   * before dragEnd.
+   * @param mouse
+   */
+  public drag(mouse: MouseEvent): void {
+    let x = mouse.offsetX;
+    let y = mouse.offsetY;
+    if (this.dragging) {
+      const dx = mouse.screenX - this.prevX;
+      const dy = mouse.screenY - this.prevY;
+      this.prevX = mouse.screenX;
+      this.prevY = mouse.screenY;
+
+      /* Left button, or primary button */
+      const mouseDir: Vec3 = this.camera.right();
+      mouseDir.scale(-dx);
+      mouseDir.add(this.camera.up().scale(dy));
+      mouseDir.normalize();
+
+      if (dx === 0 && dy === 0) {
+        return;
+      }
+
+      switch (mouse.buttons) {
+        case 1: {
+          let rotAxis: Vec3 = Vec3.cross(this.camera.forward(), mouseDir);
+          rotAxis = rotAxis.normalize();
+
+          if (this.fps) {
+            this.camera.rotate(rotAxis, GUI.rotationSpeed);
+          } else {
+            this.camera.orbitTarget(rotAxis, GUI.rotationSpeed);
+          }
+          break;
+        }
+        case 2: {
+          /* Right button, or secondary button */
+          this.camera.offsetDist(Math.sign(mouseDir.y) * GUI.zoomSpeed);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    } 
+    
+    // TODO
+    // You will want logic here:
+    // 1) To highlight a bone, if the mouse is hovering over a bone;
+    // 2) To rotate a bone, if the mouse button is pressed and currently highlighting a bone.
+
+    // mouse from screen to ndc to world
+    const ndx: number = (mouse.screenX/this.width) * 2 - 1;
+    const ndy: number = (1 - mouse.screenY/this.height) * 2 - 1;
+    const mouseWorld: Vec4 = new Vec4([ndx, ndy, -1, 1]).multiplyMat4(this.projMatrix().inverse()).multiplyMat4(this.viewMatrix().inverse());
+    mouseWorld.scale(1.0/mouseWorld.w)
+    console.log("mouse!!! " + mouseWorld.xyzw.toLocaleString());
+
+    // ray-cylinder intersection
+    const p: Vec3 = this.camera.pos();
+    const v: Vec3 = Vec3.direction(new Vec3(mouseWorld.xyz), p);
+
+    var intersection: [number, number] = [Infinity, Infinity];
+    this.animation.getScene().meshes[0].bones.forEach((bone, index) => {
+      console.log("p " + p.xyz.toLocaleString() + " v " + v.xyz.toLocaleString())
+      const pa: Vec3 = bone.position;
+      const va: Vec3 = Vec3.direction(bone.endpoint, bone.position);
+
+      const p1: Vec3 = bone.position;
+      const p2: Vec3 = bone.endpoint;
+
+      console.log("bone position " + p1.xyz.toLocaleString() + " endpoint " + p2.xyz.toLocaleString() + " axis " + va.xyz.toLocaleString())
+
+
+      const dp: Vec3 = Vec3.difference(p, pa);
+
+      const r: number = 0.1;
+
+      var temp: Vec3 = new Vec3();
+
+      const a: number = Vec3.difference(v, va.scale(Vec3.dot(v, va), temp)).squaredLength();
+      // console.log(va.xyz.toLocaleString());
+      const check: Vec3 = Vec3.difference(v, va.scale(Vec3.dot(v, va), temp));
+      const check2: Vec3 = Vec3.difference(dp, va.scale(Vec3.dot(dp, va), temp));
+      const b: number = 2.0 * Vec3.dot(check, check2);
+      const c: number = Vec3.difference(dp, va.scale(Vec3.dot(dp, va), temp)).squaredLength() - r * r;
+      // console.log("check " + check.xyz.toLocaleString() + " check2 " + check2.xyz.toLocaleString());
+      // console.log("a " + a + " b " + b + " c " + c);
+
+      var discriminant: number = b * b - 4.0 * a * c;
+
+      // console.log(discriminant);
+      if (a == 0.0 || discriminant < 0.0) {
+        return;
+      }
+
+      discriminant = Math.sqrt(discriminant);
+      // console.log(discriminant);
+      const t2 = (-b + discriminant) / (2.0 * a);
+      if(t2 <= 0.0) {
+        // console.log(t2);
+        return;
+      }
+
+      const t1 = (-b - discriminant) / (2.0 * a);
+      if(t1 > 0.0) {
+        const q1: Vec3 = Vec3.sum(p, v.scale(t1, temp));
+        // console.log("t1 " + t1 + " q1 " + q1.xyz.toLocaleString());
+        console.log(Vec3.cross(Vec3.difference(q1, p1), Vec3.difference(q1, p2)).squaredLength() + ", r^2 " + r * r);
+        if(t1 < intersection[0] && Vec3.dot(Vec3.difference(q1, p1), Vec3.difference(p2, p1)) >= 0 && Vec3.dot(Vec3.difference(q1, p1), Vec3.difference(p2, p1)) <= Vec3.dot(Vec3.difference(p2, p1), Vec3.difference(p2, p1))) {
+          intersection = [t1, index];
+          console.log("t1 intersection!!!!");
+          return;
+        }
+      }
+
+      const q2: Vec3 = Vec3.sum(p, v.scale(t2, temp));
+      // console.log("t2 " + t2 + " q2 " + q2.xyz.toLocaleString());
+      // console.log(Vec3.cross(Vec3.difference(q2, p1), Vec3.difference(q2, p2)).squaredLength() + ", r^2 " + r * r);
+      
+      if(t2 < intersection[0] && Vec3.dot(Vec3.difference(q2, p1), Vec3.difference(p2, p1)) >= 0 && Vec3.dot(Vec3.difference(q2, p1), Vec3.difference(p2, p1)) <= Vec3.dot(Vec3.difference(p2, p1), Vec3.difference(p2, p1))) {
+        intersection = [t2, index];
+        console.log("t2 intersection!!!!");
+        return;
+      }
+      
+    });
+    this.highlightedBoneIndex = intersection[1];
+  }
+
+  public getModeString(): string {
+    switch (this.mode) {
+      case Mode.edit: { return "edit: " + this.getNumKeyFrames() + " keyframes"; }
+      case Mode.playback: { return "playback: " + this.getTime().toFixed(2) + " / " + this.getMaxTime().toFixed(2); }
+    }
+  }
+
+  /**
+   * Callback function for the end of a drag event
+   * @param mouse
+   */
+  public dragEnd(mouse: MouseEvent): void {
+    this.dragging = false;
+    this.prevX = 0;
+    this.prevY = 0;
+    
+    // TODO
+    // Maybe your bone highlight/dragging logic needs to do stuff here too
+  }
+
+  /**
+   * Callback function for a key press event
+   * @param key
+   */
+  public onKeydown(key: KeyboardEvent): void {
+    switch (key.code) {
+      case "Digit1": {
+        this.animation.setScene("/static/assets/skinning/split_cube.dae");
+        break;
+      }
+      case "Digit2": {
+        this.animation.setScene("/static/assets/skinning/long_cubes.dae");
+        break;
+      }
+      case "Digit3": {
+        this.animation.setScene("/static/assets/skinning/simple_art.dae");
+        break;
+      }      
+      case "Digit4": {
+        this.animation.setScene("/static/assets/skinning/mapped_cube.dae");
+        break;
+      }
+      case "Digit5": {
+        this.animation.setScene("/static/assets/skinning/robot.dae");
+        break;
+      }
+      case "Digit6": {
+        this.animation.setScene("/static/assets/skinning/head.dae");
+        break;
+      }
+      case "Digit7": {
+        this.animation.setScene("/static/assets/skinning/wolf.dae");
+        break;
+      }
+      case "KeyW": {
+        this.camera.offset(
+            this.camera.forward().negate(),
+            GUI.zoomSpeed,
+            true
+          );
+        break;
+      }
+      case "KeyA": {
+        this.camera.offset(this.camera.right().negate(), GUI.zoomSpeed, true);
+        break;
+      }
+      case "KeyS": {
+        this.camera.offset(this.camera.forward(), GUI.zoomSpeed, true);
+        break;
+      }
+      case "KeyD": {
+        this.camera.offset(this.camera.right(), GUI.zoomSpeed, true);
+        break;
+      }
+      case "KeyR": {
+        this.animation.reset();
+        break;
+      }
+      case "ArrowLeft": {
+        this.camera.roll(GUI.rollSpeed, false);
+        break;
+      }
+      case "ArrowRight": {
+        this.camera.roll(GUI.rollSpeed, true);
+        break;
+      }
+      case "ArrowUp": {
+        this.camera.offset(this.camera.up(), GUI.zoomSpeed, true);
+        break;
+      }
+      case "ArrowDown": {
+        this.camera.offset(this.camera.up().negate(), GUI.zoomSpeed, true);
+        break;
+      }
+      case "KeyK": {
+        if (this.mode === Mode.edit) {
+            // TODO
+            // Add keyframe
+        }
+        break;
+      }      
+      case "KeyP": {
+        if (this.mode === Mode.edit && this.getNumKeyFrames() > 1)
+        {
+          this.mode = Mode.playback;
+          this.time = 0;
+        } else if (this.mode === Mode.playback) {
+          this.mode = Mode.edit;
+        }
+        break;
+      }
+      default: {
+        console.log("Key : '", key.code, "' was pressed.");
+        break;
+      }
+    }
+  }
+
+  /**
+   * Registers all event listeners for the GUI
+   * @param canvas The canvas being used
+   */
+  private registerEventListeners(canvas: HTMLCanvasElement): void {
+    /* Event listener for key controls */
+    window.addEventListener("keydown", (key: KeyboardEvent) =>
+      this.onKeydown(key)
+    );
+
+    /* Event listener for mouse controls */
+    canvas.addEventListener("mousedown", (mouse: MouseEvent) =>
+      this.dragStart(mouse)
+    );
+
+    canvas.addEventListener("mousemove", (mouse: MouseEvent) =>
+      this.drag(mouse)
+    );
+
+    canvas.addEventListener("mouseup", (mouse: MouseEvent) =>
+      this.dragEnd(mouse)
+    );
+
+    /* Event listener to stop the right click menu */
+    canvas.addEventListener("contextmenu", (event: any) =>
+      event.preventDefault()
+    );
+  }
+}
